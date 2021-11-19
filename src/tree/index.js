299c72c9c0a0924c88f677feb341d97e2e91f90f
Varela <cvarela@192.168.1.4>
2021-11-18 18:51:50 -0600

    First: (Bonus) prove that original tree are exactly same as the new one

--- src/tree/index.js
+++ src/tree/index.js
@@ -3,6 +3,20 @@ import "./index.css";
 export default function Tree() {
   return (
     <div className="tree">
+      {Array(4)
+        .fill()
+        .map((_, index) => (
+          <div className={`tree__rule--level-${index + 1}`}></div>
+        ))}
+      <p>root</p>
+      <p>&nbsp;&nbsp;&nbsp;&nbsp;ant</p>
+      <p>&nbsp;&nbsp;&nbsp;&nbsp;bear</p>
+      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cat</p>
+      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog</p>
+      <p>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant
+      </p>
+      <p>&nbsp;&nbsp;&nbsp;&nbsp;frog</p>
       <p>root</p>
       <ul>
         <li>ant</li>
