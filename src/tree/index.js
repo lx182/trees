2f651c36bf023580f266c63a68907e9846bf05dd
Varela <cvarela@192.168.1.4>
2021-11-18 21:16:26 -0600

    Next: (Bonus) redraw the tree upside down

--- src/tree/index.js
+++ src/tree/index.js
@@ -1,9 +1,10 @@
 import "./index.css";
 import TreeNode from "./TreeNode";
-const tree = require('./data.json');
+const tree = require("./data.json");
 
 export default function Tree() {
-  const {node, children} = tree;
+  const { node, children } = tree;
+
   return (
     <div className="tree">
       <TreeNode node={node} children={children} />
