d4d77a5a8976c08187ac0a9ad839ca44298e279a
Varela <cvarela@192.168.1.4>
2021-11-18 19:13:17 -0600

    First: (Bonus) add number to every line

--- src/tree/index.js
+++ src/tree/index.js
@@ -3,37 +3,24 @@ import "./index.css";
 export default function Tree() {
   return (
     <div className="tree">
-      {Array(4)
-        .fill()
-        .map((_, index) => (
-          <div className={`tree__rule--level-${index + 1}`}></div>
-        ))}
-      <p>root</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;ant</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;bear</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cat</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog</p>
-      <p>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant
-      </p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;frog</p>
-      <p>root</p>
-      <ul>
-        <li>ant</li>
-        <li>bear</li>
-        <li>
-          <ul>
-            <li>cat</li>
-            <li>dog</li>
-            <li>
-              <ul>
-                <li>elephant</li>
-              </ul>
+      <ol>
+        <li>root
+          <ol>
+            <li>ant</li>
+            <li>bear
+              <ol>
+                <li>cat</li>
+                <li>dog
+                  <ol>
+                    <li>elephant</li>
+                  </ol>
+                </li>
+              </ol>
             </li>
-          </ul>
+            <li>frog</li>
+          </ol>
         </li>
-        <li>frog</li>
-      </ul>
+      </ol>
     </div>
   );
 }
