4d90f238ebc8f2b54059dc5ab557c2a9e18b4eac
Varela <cvarela@192.168.1.4>
2021-11-18 18:42:22 -0600

    First: replace non-breaking spaces with styles

--- src/tree/index.js
+++ src/tree/index.js
@@ -4,14 +4,22 @@ export default function Tree() {
   return (
     <div className="tree">
       <p>root</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;ant</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;bear</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cat</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog</p>
-      <p>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant
-      </p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;frog</p>
+      <ul>
+        <li>ant</li>
+        <li>bear</li>
+        <li>
+          <ul>
+            <li>cat</li>
+            <li>dog</li>
+            <li>
+              <ul>
+                <li>elephant</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li>frog</li>
+      </ul>
     </div>
   );
 }
